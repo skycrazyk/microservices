89596b9d4c40ef79da979cb7d364c426700ee8ee
Dmitry Karyakin <skycrazyk@gmail.com>
2021-04-13 08:55:00 +0500

    finish arteficial microservice

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,22 @@ import React from "react";
 
 export default ({ comments }) => {
 	const renderedComments = comments.map((comment) => {
-		return <li key={comment.id}>{comment.content}</li>;
+		let content;
+
+		switch (comment.status) {
+			case "approved":
+				content = comment.content;
+				break;
+			case "pending":
+				content = "This comment is waiting moderations";
+				break;
+
+			case "rejected":
+				content = "This comment has been rejected";
+				break;
+		}
+
+		return <li key={comment.id}>{content}</li>;
 	});
 
 	return <ul>{renderedComments}</ul>;
