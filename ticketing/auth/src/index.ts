a82298203961d29f5b4a5c4c245b32a7b046fd2e
Dmitry Karyakin <skycrazyk@gmail.com>
2021-09-04 17:08:09 +0500

    finish section 7 (errors handling)

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const port = 3000
 
@@ -13,7 +16,14 @@ app.use(json())
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
+
 app.use(signupRouter)
+
+app.all('*', async () => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
