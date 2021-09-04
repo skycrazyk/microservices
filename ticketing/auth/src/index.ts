17b9f5257495d4ecec5d56e6cc73f5a6386f33a8
Dmitry Karyakin <skycrazyk@gmail.com>
2021-09-04 10:36:50 +0500

    start section 7

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express'
 import { json } from 'body-parser'
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
 
 const port = 3000
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!')
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
