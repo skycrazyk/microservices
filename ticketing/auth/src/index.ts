eb57273b2d99ce153e3b46ef4f138f5624f19bb4
Dmitry Karyakin <skycrazyk@gmail.com>
2021-09-05 11:32:24 +0500

    finish Section 8 (sign up)

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -25,6 +26,17 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}!`)
-})
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to MongoDb')
+    } catch (err) {
+        console.log(err)
+    }
+
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}!`)
+    })
+}
+
+start()
