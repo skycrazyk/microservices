3270fef0aeabf1db87f1c01a7a951f4295bd2ab5
Dmitry Karyakin <skycrazyk@gmail.com>
2021-09-22 18:03:05 +0500

    start resolve localhost request inside getInitialProps

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,7 +1,15 @@
-const Landing = () => {
+import axios from 'axios'
+
+const Landing = ({ currentUser }) => {
+    // console.log(currentUser)
     return <h1>Landing page</h1>
 }
 
-Landing.getInitialProps = () => {}
+Landing.getInitialProps = async () => {
+    // const response = await axios.get('/api/users/currentuser')
+    // return response.data
+    console.log('I WAS EXECUTED')
+    return {}
+}
 
 export default Landing
