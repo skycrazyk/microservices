8eb8f0d526bc44c5e05be4f345118fdd550f14e7
Dmitry Karyakin <skycrazyk@gmail.com>
2021-09-25 16:49:53 +0500

    Добавлен  Header, SignIn, SignOut

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -9,6 +9,7 @@ const Landing = ({ currentUser }) => {
 }
 
 Landing.getInitialProps = async (context) => {
+    console.log('Landing page!')
     const client = buildClient(context)
     const { data } = await client.get('/api/users/currentuser')
 
