0a7e45e25f1d8ea26f3bd605963d2d39be45b830
Dmitry Karyakin <skycrazyk@gmail.com>
2021-09-27 17:41:34 +0500

    start ticketing service

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError } from '@skycrazyk/gittix'
+
+export const app = express()
+
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test',
+    })
+)
+
+app.all('*', async () => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
---
+++ ticketing/tickets/src/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+
+const port = 3000
+
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined')
+    }
+
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to MongoDb')
+    } catch (err) {
+        console.log(err)
+    }
+
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}!`)
+    })
+}
+
+start()
