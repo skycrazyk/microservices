ef98e24c17de10f749a7defd1aeb760fc96a8567
Dmitry Karyakin <skycrazyk@gmail.com>
2021-10-31 17:25:58 +0500

    Добавлена публикация событий в сервис tikets

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { app } from './app'
+import { natsWrapper } from './nats-wrapper'
 
 const port = 3000
 
@@ -13,7 +14,22 @@ const start = async () => {
     }
 
     try {
+        await natsWrapper.connect(
+            'ticketing',
+            'erwerwe',
+            'http://nats-srv:4222'
+        )
+
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!')
+            process.exit()
+        })
+
+        process.on('SIGINT', () => natsWrapper.client.close())
+        process.on('SIGTERM', () => natsWrapper.client.close())
+
         await mongoose.connect(process.env.MONGO_URI)
+
         console.log('Connected to MongoDb')
     } catch (err) {
         console.log(err)
