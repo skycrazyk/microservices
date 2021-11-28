00d1902aa175ee2e0c60f15ea751423730138006
Dmitry Karyakin <skycrazyk@gmail.com>
2021-11-28 19:10:45 +0500

    добавлена обработчик order-cancelled-event

--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import { app } from '../../app'
 import mongoose from 'mongoose'
-
+import { Ticket } from '../../models/ticket'
 import { natsWrapper } from '../../nats-wrapper'
 
 it('returns a 404 if the provided id does not exist', async () => {
@@ -133,4 +133,32 @@ it('publishes an event', async () => {
         .expect(200)
 
     expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
+
+it('reject updates if the ticket is reserved', async () => {
+    const cookie = global.signin()
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'foobarbaz',
+            price: 20,
+        })
+
+    const ticket = await Ticket.findById(response.body.id)
+    ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() })
+    await ticket!.save()
+
+    const title = 'new title'
+    const price = 100
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title,
+            price,
+        })
+        .expect(400)
 })
--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
     requireAuth,
     NotFoundError,
     NotAuthorizedError,
+    BadRequestError,
 } from '@skycrazyk/gittix'
 import { Ticket } from '../models/ticket'
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher'
@@ -27,6 +28,10 @@ router.put(
 
         if (!ticket) {
             throw new NotFoundError()
+        }
+
+        if (ticket.orderId) {
+            throw new BadRequestError('Cannot edit a reserved ticket')
         }
 
         if (ticket.userId !== req.currentUser!.id) {
